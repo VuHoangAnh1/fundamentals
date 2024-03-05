bffebc77efb8a6c140a2ab545c9f733140684364
VuHoangAnh1 <112467456+VuHoangAnh1@users.noreply.github.com>
2024-03-05 08:45:58 +0700

    Update fundamentals.js

--- node-docker-gitlab-ci-main/routes/fundamentals.js
+++ node-docker-gitlab-ci-main/routes/fundamentals.js
@@ -13,11 +13,47 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/Sum2Number', function(req, res, next) {
-  res.send(Sum2Number(1,2));
+  res.send({
+    name :"Sum2Number",
+    sum  : Sum2Number(1,2)
+  });
+    
 });
 
 function Sum2Number(a,b){
   return a+b;
 }
 
+router.get('/Sum2Number', function(req, res, next) {
+  res.send({
+    name :"Sum2Number",
+    sum  : Sum2Number(1,2)
+  });
+    
+});
+function Sum2Number(a,b){
+  return a+b;
+}
+
+router.get('/Compare2Number', function(req, res, next) {
+  res.send({
+    name :"Compare2Number",
+    sum  : Compare2Number(1,2)
+  });
+});
+
+function Compare2Number(a, b) {
+  return a === b;
+}
+
+router.get('/Typeof', function(req, res, next) {
+  res.send({
+    name :"CompareTypeof2Number",
+    sum  : Typeof(1,2)
+  });
+});
+function Typeof(a) {
+  return typeof a;
+}
 module.exports = router;
+
